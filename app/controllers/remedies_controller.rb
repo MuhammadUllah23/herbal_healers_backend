a2bbbeb3b41ce011428855ef58d46183c0663770
Muhammad <muhammadhullah23@gmail.com>
2021-10-09 19:40:33 -0400

    remedy_params private method in Remedies Controller

--- app/controllers/remedies_controller.rb
+++ app/controllers/remedies_controller.rb
@@ -7,4 +7,9 @@ class RemediesController < ApplicationController
     def create
          remedy = Remedy.new(remedy_params)
     end
+
+    private
+
+    def remedy_params
+        params.require(:remedy).permit(:name, :benefits, :ingredients, :steps, :illness_id)
 end
