956ef9795885c82197190ce4fed992bbbee5a489
Muhammad <muhammadhullah23@gmail.com>
2021-10-09 21:27:11 -0400

    if and else statement in create method.

--- app/controllers/remedies_controller.rb
+++ app/controllers/remedies_controller.rb
@@ -6,10 +6,16 @@ class RemediesController < ApplicationController
 
     def create
          remedy = Remedy.new(remedy_params)
+         if remedy.save
+            render json: remedy
+         else
+            render json: {error: "Could not create Remedy"} 
+         end
     end
 
     private
 
     def remedy_params
         params.require(:remedy).permit(:name, :benefits, :ingredients, :steps, :illness_id)
+    end
 end
